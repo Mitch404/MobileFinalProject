13207b02eee22db9a10e1bcba207c46ea367a369
Russell Tremain <russtremain@gmail.com>
2018-12-09 03:47:04 -0500

    Developed UI, temperature conversion, energy calculation.

--- app/src/main/java/com/mitchrussell/finalproject/ImperialFragment.java
+++ app/src/main/java/com/mitchrussell/finalproject/ImperialFragment.java
@@ -8,26 +8,55 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class ImperialFragment extends Fragment {
     private static final String TAG = "ImperialFragment";
 
     private Button btnImperial;
+    private EditText imperialVolume;
+    private EditText imperialTemperature;
+    private EditText imperialOutput;
+    private double fluidOunceToMillilitre = 28.413125;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_imperial, container, false);
-        btnImperial = (Button) view.findViewById(R.id.imperialButton);
+        btnImperial = (Button) view.findViewById(R.id.imperialCalculateButton);
+        imperialVolume = view.findViewById(R.id.imperialVolumeEditText);
+        imperialTemperature = view.findViewById(R.id.imperialTemperatureEditText);
+        imperialOutput = view.findViewById(R.id.imperialResult);
 
         btnImperial.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(ImperialFragment.this.getActivity(), "Testing Imperial Button Click", Toast.LENGTH_SHORT).show();
+
+            try{
+                int imperialVolumeInteger = Integer.parseInt(imperialVolume.getText().toString());
+                int imperialTemperatureInteger = Integer.parseInt(imperialTemperature.getText().toString());
+
+                double metricVolumeStopGap = imperialVolumeInteger * fluidOunceToMillilitre;
+
+                double metricTemperatureStopGap = ((imperialTemperatureInteger-32)*(5/9));
+
+                int imperialResult = (int) Math.round(metricTemperatureStopGap * metricVolumeStopGap);
+
+
+
+                Toast.makeText(ImperialFragment.this.getActivity(), "This much energy in kcals: "
+                        + imperialResult, Toast.LENGTH_LONG).show();
+                imperialOutput.setText(imperialResult);
+
+
+            }
+            catch(Exception e){
+                Toast.makeText(ImperialFragment.this.getActivity(), "A number is invalid.", Toast.LENGTH_LONG).show();
+            }
+
             }
         });
-
         return view;
     }
 
--- app/src/main/java/com/mitchrussell/finalproject/MetricFragment.java
+++ app/src/main/java/com/mitchrussell/finalproject/MetricFragment.java
@@ -8,26 +8,47 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MetricFragment extends Fragment {
     private static final String TAG = "MetricFragment";
 
     private Button btnMetric;
+    private EditText metricVolume;
+    private EditText metricTemperature;
+    private EditText metricOutput;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_metric, container, false);
-        btnMetric = (Button) view.findViewById(R.id.metricButton);
+        btnMetric = (Button) view.findViewById(R.id.metricCalculateButton);
+        metricVolume = view.findViewById(R.id.metricVolumeEditText);
+        metricTemperature = view.findViewById(R.id.metricTemperatureEditText);
+        metricOutput = view.findViewById(R.id.metricResult);
+
 
         btnMetric.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MetricFragment.this.getActivity(), "Testing Metric Button Click", Toast.LENGTH_SHORT).show();
+
+            try{
+                int metricVolumeInteger = Integer.parseInt(metricVolume.getText().toString());
+                int metricTemperatureInteger = Integer.parseInt(metricTemperature.getText().toString());
+
+                int metricResult = metricTemperatureInteger * metricVolumeInteger;
+
+                Toast.makeText(MetricFragment.this.getActivity(), "This much energy in kcals: " + metricResult, Toast.LENGTH_LONG).show();
+                metricOutput.setText(metricResult);
+
+
+            }
+            catch(Exception e){
+                Toast.makeText(MetricFragment.this.getActivity(), "A number is invalid.", Toast.LENGTH_LONG).show();
+            }
             }
         });
-
         return view;
     }
 
